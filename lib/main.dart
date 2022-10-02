9b8a4aeaa7af80db320e12fd28d5c6e755fc2bfc
gengsR <rahmaputru997@gmail.com>
2022-10-02 11:38:06 +0700

    Fix ConvertSuhu

--- lib/main.dart
+++ lib/main.dart
@@ -1,160 +1,274 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Suhu Converter',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'Konverter Suhu'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController etInput = new TextEditingController();
-
-  //Menentukan variabel input
+  var items = ['Kelvin', 'Reamor', 'Farenheit', 'Semua'];
+  String dropdownvalue = 'Kelvin';
+  String? selectedValue;
   double celcius = 0;
   double kelvin = 0;
-  double fahrenheit = 0;
-  double reamur = 0;
-
-  // Melakukan konversi
+  double reamor = 0;
+  double farenheit = 0;
+  double output = 0;
+  double output1 = 0;
+  double output2 = 0;
+  double output3 = 0;
+  List<String> listViewItem1 = <String>[];
+  List<String> listViewItem2 = <String>[];
+  List<String> listViewItem3 = <String>[];
+  List<String> listViewItem4 = <String>[];
+  TextEditingController inputSuhu = TextEditingController();
   konversi() {
-    //dengan state
     setState(() {
-      celcius = double.parse(etInput.text);
-      reamur = 4 / 5 * celcius;
-      fahrenheit = (9 / 5 * celcius) + 32;
-      kelvin = 273.15 + celcius;
+      celcius = double.parse(inputSuhu.text);
+      if (dropdownvalue == 'Kelvin') {
+        output = 273.15 + celcius;
+        reamor = 0;
+        farenheit = 0;
+        kelvin = output;
+      } else if (dropdownvalue == 'Reamor') {
+        output = 4 / 5 * celcius;
+        reamor = output;
+        farenheit = 0;
+        kelvin = 0;
+      } else if (dropdownvalue == 'Farenheit') {
+        output = (9 / 5 * celcius) + 32;
+        reamor = 0;
+        farenheit = output;
+        kelvin = 0;
+      } else {
+        output1 = 273.15 + celcius;
+        output2 = 4 / 5 * celcius;
+        output3 = (9 / 5 * celcius) + 32;
+        reamor = output2;
+        farenheit = output3;
+        kelvin = output1;
+      }
+      listViewItem1.add('$reamor');
+      listViewItem2.add('$kelvin');
+      listViewItem3.add('$farenheit');
+      listViewItem4.add('$celcius');
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Container(
-        margin: EdgeInsets.all(8),
-        child: Column(
-          children: [
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konventer Suhu"),
+        ),
+        body: Column(
+          children: <Widget>[
             Expanded(
+              flex: 2,
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(child: inputan()),
+                    Container(
+                      child: dropDown(),
+                    ),
+                    Container(
+                      child: outputSuhu(),
+                    ),
+                    Container(
+                      child: buttonSubmit(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Expanded(
+              flex: 0,
+              child: Text(
+                'Riwayat Konversi',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Expanded(
+              flex: 2,
+              child: listHasil(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  inputan() {
+    return TextFormField(
+      controller: inputSuhu,
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        border: UnderlineInputBorder(),
+        labelText: 'Masukan Suhu Dalam Celcius',
+      ),
+    );
+  }
+
+  dropDown() {
+    return DropdownButton(
+      value: dropdownvalue,
+      icon: const Icon(Icons.keyboard_arrow_down),
+      items: items.map((String items) {
+        return DropdownMenuItem(
+          value: items,
+          child: Text(items),
+        );
+      }).toList(),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownvalue = newValue!;
+        });
+      },
+    );
+  }
+
+  outputSuhu() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              const Text('Suhu dalam Reamur',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                  textAlign: TextAlign.center),
+              const SizedBox(height: 20),
+              Text(
+                "$reamor",
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              const Text(
+                'Suhu dalam \nKelvin',
+                style: TextStyle(fontSize: 15),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "$kelvin",
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              const Text('Suhu dalam \nFahrenheit',
+                  style: TextStyle(fontSize: 15), textAlign: TextAlign.center),
+              const SizedBox(height: 20),
+              Text(
+                "$farenheit",
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  buttonSubmit() {
+    return SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          child: const Text('Submit'),
+          onPressed: () => konversi(),
+        ));
+  }
+
+  listHasil() {
+    return SizedBox(
+      height: 120,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: listViewItem4.length,
+        itemBuilder: (context, index) => Container(
+          decoration:
+              BoxDecoration(border: Border.all(color: Colors.lightBlueAccent)),
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: Row(children: <Widget>[
+            Expanded(
+              flex: 4,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  TextFormField(
-                    keyboardType: TextInputType.number,
-                    controller: etInput,
-                    decoration: InputDecoration(
-                      hintText: "Masukkan nilai suhu celcius",
-                    ),
+                children: [Text((index + 1).toString())],
+              ),
+            ),
+            Expanded(
+              flex: 10,
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Celcius',
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, height: 2),
+                      ),
+                      Text(
+                        'Reamur\t',
+                      ),
+                      Text('Kelvin\t'),
+                      Text('Fahrenheit\t'),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ],
                   ),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Text(
-                              "Reamur",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              "$reamur",
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Text(
-                              "Fahrenheit",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              "$fahrenheit",
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Text(
-                              "Kelvin",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              "$kelvin",
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 50,
-                  ),
-                  TextButton(
-                    onPressed: konversi,
-                    child: Text(
-                      'Konversi',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
+                  Column(children: const [
+                    Text('=\t'),
+                    Text('=\t'),
+                    Text('=\t'),
+                    Text('=\t'),
+                  ]),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(listViewItem4.elementAt(index)),
+                        Text(listViewItem1.elementAt(index)),
+                        Text(listViewItem2.elementAt(index)),
+                        Text(listViewItem3.elementAt(index)),
+                      ]),
                 ],
               ),
             ),
-          ],
+          ]),
         ),
       ),
     );
